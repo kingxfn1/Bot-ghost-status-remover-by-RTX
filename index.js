21e3f609f86e602a1022935586bfd74d420e6dc8
kingxfn1 <159289738+kingxfn1@users.noreply.github.com>
2024-02-07 19:19:49 +0100

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["**Helping** and **Serving** Regional Ottawa Roleplay."];
 
 
 let currentIndex = 0;
